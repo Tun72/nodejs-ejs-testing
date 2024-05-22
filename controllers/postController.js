d9cd005ea69ff7e20bdd3ff560b0dfbc38e0bcb2
Tun72 <tunt72553@gmail.com>
2024-05-22 20:14:29 +0630

    MYSQL

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,15 +1,25 @@
-const posts = [];
+// const posts = [];
+const Post = require("../models/postModel");
 
 exports.createPost = (req, res) => {
   const { title, description, photo } = req.body;
-  posts.push({
-    id: Math.random(),
-    title,
-    description,
-    photo,
-  });
-  console.log(title);
-  res.redirect("/");
+  const post = new Post(title, description, photo);
+
+  post
+    .setPost()
+    .then((data) => {
+      console.log(data);
+      res.redirect("/");
+    })
+    .catch((e) => {
+      res.write(e);
+    });
+  // posts.push({
+  //   id: Math.random(),
+  //   title,
+  //   description,
+  //   photo,
+  // });
 };
 
 exports.renderCreatePage = (req, res) => {
@@ -18,12 +28,30 @@ exports.renderCreatePage = (req, res) => {
 
 exports.renderHomePage = (req, res) => {
   // res.sendFile(path.join(__dirname, "..", "views", "homepage.html"));
-  res.render("home", { title: "home", posts });
+  Post.getAllPosts()
+    .then(([posts]) => {
+      // console.log(posts);
+      res.render("home", { title: "home", posts });
+    })
+    .catch((e) => {
+      console.log(e);
+      return res.send(e.message);
+    });
 };
 
 exports.getPost = (req, res) => {
   const postId = Number(req.params.postId);
+
   console.log(postId);
-  const post = posts.find((post) => post.id === postId);
-  res.render("detail", { title: "detail", post });
+  if (!postId) return res.write("ERROR");
+
+  Post.getSinglePost(postId)
+    .then(([post]) => {
+      if (post.length === 0) throw new Error("Not Found");
+      res.render("detail", { title: "detail", post: post[0] });
+    })
+    .catch((e) => {
+      // console.log(e);
+      return res.send(e.message);
+    });
 };
